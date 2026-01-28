5af8d1c5fd2096d95d1020a56a57064f0d9a5e41
choudharyashutosh9045 <choudharyashutosh9045@gmail.com>
2026-01-28 17:19:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -221,7 +221,7 @@ def generate_invoice_pdf(row: dict, pdf_path: str):
         try:
             img = ImageReader(logo_path)
             logo_w = 60 * mm
-            logo_h = 33 * mm
+            logo_h = 30 * mm
             logo_x = LM + 6 * mm
             logo_y = H - TM - 38 * mm
             c.drawImage(img, logo_x, logo_y, width=logo_w, height=logo_h, mask="auto", preserveAspectRatio=True)
